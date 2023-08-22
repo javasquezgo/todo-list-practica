49be64dd0130b161b184aa8023fadedfa0a812ed
Alejandro Vasquez <josejaguer12@gmail.com>
2023-08-21 21:55:17 -0600

    Avanzando en el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, ref } from "vue";
+import AddTask from "./components/AddTask.vue";
+
+const tasks = ref([]);
+
+const task = reactive({
+  tarea: "",
+  state: false,
+});
+
+const doneTasks = tasks.value.filter((task) => {
+  return task.state === true;
+}).length;
+
+const totalTasks = tasks.value.length;
+
+const addNewTask = (text) => {
+  tasks.value.push({
+    tarea: text,
+    state: false,
+  });
+
+  Object.assign(task, {
+    tarea: "",
+    state: false,
+  });
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="container">
+    <h1>Lista de Tareas</h1>
+    <h2>Se han completado {{ doneTasks }} de {{ totalTasks }}</h2>
+    <AddTask v-model:tarea="task.tarea" @add-new-task="addNewTask" />
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/AddTask.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  tarea: {
+    type: String,
+    required: true,
+  },
+});
+const emit = defineEmits(["update:tarea", "add-new-task"]);
+</script>
+
+<template>
+  <div class="task-add">
+    <input
+      type="text"
+      placeholder="¿Qué tienes planeado para hoy?"
+      id="tarea"
+      :value="tarea"
+      @input="$emit('update:tarea', $event.target.value)"
+    />
+    <button @click="$emit('add-new-task', tarea)">Agregar Tarea</button>
+  </div>
+</template>
